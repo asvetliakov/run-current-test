b1d52c0fc6e5a1714df28cc2a98a012062569f21
Alexey Svetliakov <asvetliakov@teledini.com>
2017-10-26 00:09:50 +0200

    Support multi root

--- src/extension.ts
+++ src/extension.ts
@@ -17,17 +17,27 @@ const defaultBlockIdentifiers = [
 
 let terminal: vscode.Terminal | undefined;
 
+function getConfiguration(uri: vscode.Uri): vscode.WorkspaceConfiguration {
+    return vscode.workspace.getConfiguration("runCurrentTest", uri);
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    const configuration = vscode.workspace.getConfiguration("runCurrentTest");
-    
+
     const runTestComamnd = (editor: vscode.TextEditor, command: string) => {
+        const document = editor.document;
+        const configuration = getConfiguration(document.uri);
+
+        const workspaceFoler = vscode.workspace.getWorkspaceFolder(document.uri);
+        // We don't need root for all scenarios, relative paths work too
+        const workspaceRoot = workspaceFoler ? workspaceFoler.uri.fsPath || "" : "";
+
         // Do nothing for non real file editor
-        if (!editor.document.fileName || !vscode.workspace.rootPath) {
+        if (!document.fileName) {
             return;
         }
-        
+
         if (editor.document.isDirty) {
             if (configuration.get("autoSave", true)) {
                 editor.document.save();
@@ -36,30 +46,30 @@ export function activate(context: vscode.ExtensionContext) {
                 return;
             }
         }
-        
+
         const runCurrentTestCommand = configuration.get(command, undefined);
         if (!runCurrentTestCommand) {
             vscode.window.showErrorMessage(`Please set runCurrentTest.${command} to your test command first`);
             return;
         }
-        
+
         try {
-            const sourceFile = createSourceFile("test.tsx", editor.document.getText(), ScriptTarget.ES2015, true, ScriptKind.TSX);
+            const sourceFile = createSourceFile("test.tsx", document.getText(), ScriptTarget.Latest, true, ScriptKind.TSX);
             const blocks = getTestBlocks(sourceFile, configuration.get("testBlockIdentifiers", defaultBlockIdentifiers));
             const block = findTestBlockForLineAndCharacter(sourceFile, blocks, editor.selection.active.line, editor.selection.active.character);
             const finalCommand = resolveCommand(runCurrentTestCommand, {
                 testBlock: block,
-                testFilePath: editor.document.fileName,
+                testFilePath: document.fileName,
                 testNameSeparator: configuration.get("testNameSeparator", "\\s"),
                 unknownTestNameLiteral: configuration.get("unknownTestNameLiteral", ".*"),
-                workspaceRoot: vscode.workspace.rootPath
+                workspaceRoot: workspaceRoot
             });
-            
+
             // dispose previous terminal instance
             if (terminal) {
                 terminal.dispose();
             }
-            
+
             terminal = vscode.window.createTerminal("Current test run");
             terminal.sendText(finalCommand, true);
             terminal.show(true);
@@ -69,15 +79,15 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
     }
-    
+
     const command1 = vscode.commands.registerTextEditorCommand("runCurrentTest.run", editor => {
         runTestComamnd(editor, "run");
     });
-    
+
     const command2 = vscode.commands.registerTextEditorCommand("runCurrentTest.runAndUpdateSnapshots", editor => {
         runTestComamnd(editor, "runAndUpdateSnapshots");
     });
-    
+
     context.subscriptions.push(command1);
     context.subscriptions.push(command2);
 }
